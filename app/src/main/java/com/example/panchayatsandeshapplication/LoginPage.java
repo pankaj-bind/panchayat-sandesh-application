e65dde65ad7ec81e5c1b687d0a316575bda4589b
Pankaj Kumar Bind <pankajbind30@gmail.com>
2023-10-09 20:59:41 +0530

    login page added

--- app/src/main/java/com/example/panchayatsandeshapplication/LoginPage.java
+++ app/src/main/java/com/example/panchayatsandeshapplication/LoginPage.java
@@ -3,6 +3,7 @@ package com.example.panchayatsandeshapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class LoginPage extends AppCompatActivity {
 
@@ -10,5 +11,8 @@ public class LoginPage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_page);
+    }
+
+    public void signUpClicked(View view) {
     }
 }
