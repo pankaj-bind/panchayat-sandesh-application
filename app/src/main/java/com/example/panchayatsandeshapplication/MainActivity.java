44cd985c8c80a8a2f313864ab00fe09c653b98f6
Pankaj Kumar Bind <pankajbind30@gmail.com>
2023-10-09 19:33:12 +0530

    splash screen created

--- app/src/main/java/com/example/panchayatsandeshapplication/MainActivity.java
+++ app/src/main/java/com/example/panchayatsandeshapplication/MainActivity.java
@@ -1,14 +1,6 @@
 package com.example.panchayatsandeshapplication;
 
-import androidx.appcompat.app.AppCompatActivity;
+import android.app.Activity;
 
-import android.os.Bundle;
-
-public class MainActivity extends AppCompatActivity {
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
+public class MainActivity extends Activity {
 }
