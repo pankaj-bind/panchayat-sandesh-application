0fa95744efd2855d97a5cd8832ee00d2564d5ec2
Pankaj Kumar Bind <pankajbind30@gmail.com>
2023-10-16 15:18:02 +0530

    Sign Up page added

--- app/src/main/java/com/example/panchayatsandeshapplication/ChooseAccountType.java
+++ app/src/main/java/com/example/panchayatsandeshapplication/ChooseAccountType.java
@@ -3,17 +3,12 @@ package com.example.panchayatsandeshapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.view.View;
 
-public class LoginPage extends AppCompatActivity {
+public class ChooseAccountType extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login_page);
-    }
-
-    public void signUpClicked(View view) {
-        
+        setContentView(R.layout.activity_choose_account_type);
     }
 }
